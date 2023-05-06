c0da91857969b62d834083bfd4a0c26ac6cd9c13
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-05 21:29:10 -0400

    Use forEach in cloneSections

--- src/main.js
+++ src/main.js
@@ -106,16 +106,16 @@ function switchToHome() {
 }
 
 function cloneSections() {
-  for (var i = 0; i < savedCovers.length; i++) {
-      savedCoversSection.innerHTML += `
-      <section class="mini-cover" id="${savedCovers[i].id}">
-        <img class="cover-image" src="${savedCovers[i].coverImg}">
-        <h2 class="cover-title">${savedCovers[i].title}</h2>
-        <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
-        <img class="price-tag" src="./assets/price.png">
-        <img class="overlay" src="./assets/overlay.png">
-      </section>`
-  }
+  savedCovers.forEach((cover) => {
+    savedCoversSection.innerHTML += `
+    <section class="mini-cover" id="${cover.id}">
+      <img class="cover-image" src="${cover.coverImg}">
+      <h2 class="cover-title">${cover.title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${cover.tagline1}</span> and <span class="tagline-2">${cover.tagline2}</span></h3>
+      <img class="price-tag" src="./assets/price.png">
+      <img class="overlay" src="./assets/overlay.png">
+    </section>`
+  })
 }
 
 function switchToSaved() {
